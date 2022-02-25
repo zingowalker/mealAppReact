86f26cf0f1e24b4ecca6a19f84135d1d24f059f4
Tsetan Dorje <tsetangrace@gmail.com>
2022-02-25 18:10:23 +0530

    added components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,97 +1,65 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import { Text, Image, View } from 'react-native';
-import { Card } from 'react-native-paper';
-import { SvgXml } from 'react-native-svg';
+import React from "react";
+import { SvgXml } from "react-native-svg";
 
-import star from '../../../../assets/star';
-import open from '../../../../assets/open';
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-    background-color: ${(props) => props.theme.colors.bg.primary};
-    padding: ${(props) => props.theme.space[3]};
-`;
-
-const Address = styled(Text)`
-    font-family: ${(props) => props.theme.fonts.body};
-    font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Title = styled(Text)`
-    font-family: ${(props) => props.theme.fonts.heading};
-    font-size: ${(props) => props.theme.fontSizes.body};
-    color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-    padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-    flex-direction: row;
-    padding-top: ${(props) => props.theme.space[2]};
-    padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-    flex-direction: row;
-    align-items: center;
-`;
-
-const SectionEnd = styled.View`
-    flex: 1;
-    flex-direction: row;
-    justify-content: flex-end;
-`;
-
-
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
-    const {
-        name = 'Gesmo Restaurant',
-        icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
-        photos = [
-            'https://media-cdn.tripadvisor.com/media/photo-s/1d/cc/bd/e4/caption.jpg'
-        ],
-        address = 'Leh, UT - Ladakh 194101',
-        isOpenNow = true,
-        rating = 3,
-        isClosedTemporarily = true,
-    } = restaurant;
+  const {
+    name = "Gesmo Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://images.unsplash.com/photo-1631515242808-497c3fbd3972?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmlyeWFuaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    ],
+    address = "Fort Road, Leh, UT - Ladakh 194101",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
-    const ratingArray = Array.from(new Array(Math.floor(rating)));
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
 
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
 
-    return (
-        <RestaurantCard elevation={5}>
-            <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Info>
-                <Title>{name}</Title>
-                <Section>
-                    <Rating>
-                        {ratingArray.map(() => (
-                            <SvgXml xml={star} width={20} height={20} />
-                        ))}
-                    </Rating>
-                    <SectionEnd>
-                        {isClosedTemporarily && (
-                            <Text variant="label" style={{ color: "red" }}>
-                                CLOSED TEMPORARILY
-                            </Text>
-                        )}
-                        <View style={{ paddingLeft: 16 }} />
-                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-                        <View style={{ paddingLeft: 16 }} />
-                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-                    </SectionEnd>
-                </Section>
-                <Address>{address}</Address>
-            </Info>
-        </RestaurantCard>
-    );
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+            
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
 };
